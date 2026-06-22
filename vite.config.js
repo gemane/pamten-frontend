acd3def7b3f89a0516ff9cbae531c39150b9037b
Gerold Neuwirt <gerold.neuwirt@gmail.com>
2026-06-22 21:41:54 +0200

    Split bundle into manual chunks, silence cytoscape size warning

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor:    ['react', 'react-dom'],
+          cytoscape: ['cytoscape', 'cytoscape-cola'],
+          icons:     ['react-icons'],
+        },
+      },
+    },
+  },
 })
